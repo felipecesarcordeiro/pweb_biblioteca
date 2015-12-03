a4e4f25959d9bb36ee0dc470666146513dd28d15
LADOSSIFPB <ladossifpb@gmail.com>
2015-12-03 11:41:02 -0300

    Remanejamento

---
+++ src/br/edu/ifpb/Biblioteca_pweb/Entidades/Usuario.java
@@ -0,0 +1,21 @@
+package br.edu.ifpb.Biblioteca_pweb.Entidades;
+
+public class Usuario 
+{
+	private String nome;
+	private String senha;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
